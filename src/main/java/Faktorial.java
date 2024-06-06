daf1c3c190a55a64248304420c6a9b736694bd8f
Avisenna21 <91260446+Avisenna21@users.noreply.github.com>
2024-06-06 15:44:45 +0700

    Update Faktorial.java

--- src/main/java/Faktorial.java
+++ src/main/java/Faktorial.java
@@ -10,3 +10,14 @@ public class Faktorial {
     }
 }
 
+public class Faktorial {
+    public int hitungFaktorial(int n) {
+        if (n == 0) {
+            return 1;
+        } else {
+            return n * hitungFaktorial(n - 1);
+        }
+    }
+}
+
+
